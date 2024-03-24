fb26f695972fa62e1923a4b51471319cc565dfc6
Mikekimm <mike4kimani@gmail.com>
2024-03-24 12:22:24 -0700

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+"""
+Fetches https://alu-intranet.hbtn.io/status and prints the response.
+"""
 import urllib.request
-import sys
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    try:
-        with urllib.request.urlopen(url) as response:
-            html_content = response.read()
-            utf8_content = html_content.decode('utf-8')
+    url = "https://alu-intranet.hbtn.io/status"
 
-            print("Body response:")
-            print("\t- type:", type(html_content))
-            print("\t- content:", html_content)
-            print("\t- utf8 content:", utf8_content)
-    except urllib.error.HTTPError as e:
-        print("HTTP Error:", e.code, e.reason, file=sys.stderr)
+    # Create a request object
+    with urllib.request.urlopen(url) as response:
+        # Read the response content
+        content = response.read()
+
+        # Decode the content to UTF-8
+        utf8_content = content.decode('utf-8')
+
+        # Display response
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
