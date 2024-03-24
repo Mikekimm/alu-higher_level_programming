dbbae4e016948d64dbd9110237c73be6935c2dd0
Mikekimm <mike4kimani@gmail.com>
2024-03-24 12:57:05 -0700

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""
-Fetches https://alu-intranet.hbtn.io/status and prints the response.
-"""
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
+
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-
-    # Create a request object
-    with urllib.request.urlopen(url) as response:
-        # Read the response content
-        content = response.read()
-
-        # Decode the content to UTF-8
-        utf8_content = content.decode('utf-8')
-
-        # Display response
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(utf8_content))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
