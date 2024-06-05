ee05bc1c1d926a0d4906525be42dfeea3d1f8bf8
Mikekimm <mike4kimani@gmail.com>
2024-06-05 15:44:06 +0200

    00

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const python = 'Python is cool';
-const c = 'C is fun';
-const javas = 'JavaScript is amazing';
-console.log(c);
-console.log(python);
-console.log(javas);
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
